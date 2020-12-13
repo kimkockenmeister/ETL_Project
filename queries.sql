fa2687bbda9536edc4c99b16234c7180367580ea
Jingwen Sun <70722196+jis56@users.noreply.github.com>
2020-12-13 13:49:44 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,3 +10,17 @@ SELECT disneyplus.imdb_id
 FROM disneyplus
 INNER JOIN imdb
 ON disneyplus.imdb_id = imdb.imdb_id;
+
+SELECT disneyplus.imdb_id
+,disneyplus.title
+,disneyplus.imdb_rating
+,disneyplus.imdb_votes
+,imdb.eighteen_avg_vote
+,imdb.thirty_avg_vote
+,imdb.fourtyfive_avg_vote
+,imdb.us_voters_rating
+,imdb.non_us_voters_rating
+FROM disneyplus
+INNER JOIN imdb
+ON disneyplus.imdb_id = imdb.imdb_id
+ORDER BY disneyplus.imdb_rating;
