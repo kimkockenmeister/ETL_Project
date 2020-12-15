4f7a3f10660282f82f9d641f32c1a235d042c11e
Jingwen Sun <70722196+jis56@users.noreply.github.com>
2020-12-14 20:12:50 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -11,6 +11,8 @@ FROM disneyplus
 INNER JOIN imdb
 ON disneyplus.imdb_id = imdb.imdb_id;
 
+
+--Looking at Disney movies sorted by highest ratings
 SELECT disneyplus.imdb_id
 ,disneyplus.title
 ,disneyplus.imdb_rating
@@ -24,3 +26,18 @@ FROM disneyplus
 INNER JOIN imdb
 ON disneyplus.imdb_id = imdb.imdb_id
 ORDER BY disneyplus.imdb_rating;
+
+--Looking at Star Wars movie ratings
+SELECT disneyplus.imdb_id
+,disneyplus.title
+,disneyplus.imdb_rating
+,disneyplus.imdb_votes
+,imdb.eighteen_avg_vote
+,imdb.thirty_avg_vote
+,imdb.fourtyfive_avg_vote
+,imdb.us_voters_rating
+,imdb.non_us_voters_rating
+FROM disneyplus
+INNER JOIN imdb
+ON disneyplus.imdb_id = imdb.imdb_id
+WHERE disneyplus.title LIKE '%sode%';
